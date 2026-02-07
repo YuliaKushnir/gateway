325a07a8fd294ca507056711fce0af0d4369763c
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-02-07 04:03:29 +0200

    Commented unneсessary dependencies

--- src/main/java/org/example/gateway/config/SecurityConfig.java
+++ src/main/java/org/example/gateway/config/SecurityConfig.java
@@ -1,27 +1,22 @@
 package org.example.gateway.config;
 
-//import org.example.gateway.service.OAuth2LoginSuccessHandler;
+import org.example.gateway.service.OAuth2LoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
-import org.springframework.web.server.ServerWebExchange;
-
-import javax.naming.AuthenticationException;
 
 @EnableWebFluxSecurity
 @Configuration
 public class SecurityConfig {
 
-    //private final OAuth2LoginSuccessHandler successHandler;
+    private final OAuth2LoginSuccessHandler successHandler;
 
-    //public SecurityConfig(OAuth2LoginSuccessHandler successHandler) {
-   //     this.successHandler = successHandler;
-    //}
+    public SecurityConfig(OAuth2LoginSuccessHandler successHandler) {
+        this.successHandler = successHandler;
+    }
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
@@ -34,22 +29,8 @@ public class SecurityConfig {
                         .anyExchange().authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
-                        .authenticationSuccessHandler(
-                                new RedirectServerAuthenticationSuccessHandler("http://34.118.68.15.nip.io/")
-                        )
+                        .authenticationSuccessHandler(successHandler)
                 );
-
-
-
-//                .oauth2Login(oauth2 -> oauth2
-//                                .authenticationSuccessHandler(successHandler)
-//                )
-//                .exceptionHandling(exceptions -> exceptions
-//                        .authenticationEntryPoint((exchange, ex) -> {
-//                            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-//                            return exchange.getResponse().setComplete();
-//                        })
-//                );
 
         return http.build();
     }
--- src/main/java/org/example/gateway/service/OAuth2LoginSuccessHandler.java
+++ src/main/java/org/example/gateway/service/OAuth2LoginSuccessHandler.java
@@ -1,52 +1,52 @@
-//package org.example.gateway.service;
-//
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-//import org.springframework.security.web.server.WebFilterExchange;
-//import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
-//import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.reactive.function.client.WebClient;
-//import reactor.core.publisher.Mono;
-//
-//import java.util.Map;
-//
-//@Component
-//public class OAuth2LoginSuccessHandler implements ServerAuthenticationSuccessHandler {
-//
-//    private final WebClient webClient;
-//
-//    private final RedirectServerAuthenticationSuccessHandler redirectHandler =
-//            new RedirectServerAuthenticationSuccessHandler("http://34.118.68.15.nip.io");
-//
-//    public OAuth2LoginSuccessHandler(WebClient webClient) {
-//        this.webClient = webClient;
-//    }
-//
-//    @Override
-//    public Mono<Void> onAuthenticationSuccess(
-//            WebFilterExchange webFilterExchange,
-//            Authentication authentication
-//    ) {
-//
-//        OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
-//
-//        return webClient.post()
-//                .uri("http://posts-service:8080/api/internal/user/oauth")
-//                .bodyValue(Map.of(
-//                        "email", oidcUser.getEmail(),
-//                        "name", oidcUser.getFullName()
-//                ))
-//                .retrieve()
-//                .bodyToMono(Long.class)
-//                .flatMap(userId ->
-//                        webFilterExchange.getExchange()
-//                                .getSession()
-//                                .doOnNext(session ->
-//                                        session.getAttributes().put("userId", userId)
-//                                )
-//                                .then()
-//                )
-//                .then(redirectHandler.onAuthenticationSuccess(webFilterExchange, authentication));
-//    }
-//}
+package org.example.gateway.service;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.server.WebFilterExchange;
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+@Component
+public class OAuth2LoginSuccessHandler implements ServerAuthenticationSuccessHandler {
+
+    private final WebClient webClient;
+
+    private final RedirectServerAuthenticationSuccessHandler redirectHandler =
+            new RedirectServerAuthenticationSuccessHandler("http://34.118.68.15.nip.io");
+
+    public OAuth2LoginSuccessHandler(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    @Override
+    public Mono<Void> onAuthenticationSuccess(
+            WebFilterExchange webFilterExchange,
+            Authentication authentication
+    ) {
+
+        OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
+
+        return webClient.post()
+                .uri("http://posts-service:8080/api/internal/user/oauth")
+                .bodyValue(Map.of(
+                        "email", oidcUser.getEmail(),
+                        "name", oidcUser.getFullName()
+                ))
+                .retrieve()
+                .bodyToMono(Long.class)
+                .flatMap(userId ->
+                        webFilterExchange.getExchange()
+                                .getSession()
+                                .doOnNext(session ->
+                                        session.getAttributes().put("userId", userId)
+                                )
+                                .then()
+                )
+                .then(redirectHandler.onAuthenticationSuccess(webFilterExchange, authentication));
+    }
+}
