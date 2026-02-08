98266efbfc2a88cd1efe468f1ac5001548e3cb66
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-02-08 19:49:36 +0200

    Changed path

--- src/main/java/org/example/gateway/config/SecurityConfig.java
+++ src/main/java/org/example/gateway/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package org.example.gateway.config;
 
 import org.example.gateway.service.OAuth2LoginSuccessHandler;
+import org.springframework.cloud.gateway.filter.headers.ForwardedHeadersFilter;
+import org.springframework.cloud.gateway.filter.headers.XForwardedHeadersFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -42,5 +44,15 @@ public class SecurityConfig {
         ForwardedHeaderTransformer transformer = new ForwardedHeaderTransformer();
         transformer.setRemoveOnly(false);
         return transformer;
+    }
+
+    @Bean
+    public ForwardedHeadersFilter forwardedHeadersFilter() {
+        return new ForwardedHeadersFilter();
+    }
+
+    @Bean
+    public XForwardedHeadersFilter xForwardedHeadersFilter() {
+        return new XForwardedHeadersFilter();
     }
 }
