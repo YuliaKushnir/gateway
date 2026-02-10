bf82de731c1a7d856a54254b6740a90993c5a1e5
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-02-10 16:26:41 +0200

    Changed oauth settings

--- src/main/java/org/example/gateway/config/SecurityConfig.java
+++ src/main/java/org/example/gateway/config/SecurityConfig.java
@@ -1,15 +1,15 @@
 package org.example.gateway.config;
 
 import org.example.gateway.service.OAuth2LoginSuccessHandler;
-import org.springframework.cloud.gateway.filter.headers.ForwardedHeadersFilter;
-import org.springframework.cloud.gateway.filter.headers.XForwardedHeadersFilter;
+//import org.springframework.cloud.gateway.filter.headers.ForwardedHeadersFilter;
+//import org.springframework.cloud.gateway.filter.headers.XForwardedHeadersFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+//import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.server.adapter.ForwardedHeaderTransformer;
+//import org.springframework.web.server.adapter.ForwardedHeaderTransformer;
 
 @EnableWebFluxSecurity
 @Configuration
