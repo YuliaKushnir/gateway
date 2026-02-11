06e252f5439191ee11dad6d23daa7b9b00e34e27
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-02-11 20:54:53 +0200

    Changed profile endpoint

--- src/main/java/org/example/gateway/controller/ProfileController.java
+++ src/main/java/org/example/gateway/controller/ProfileController.java
@@ -1,11 +1,11 @@
 package org.example.gateway.controller;
 
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-//import org.springframework.web.server.WebSession;
+import org.springframework.web.server.WebSession;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,11 +21,18 @@ public class ProfileController {
 //    }
 
     @GetMapping("/api/profile")
-    public Mono<Map<String, Object>> profile() {
-        Map<String, Object> profile = new HashMap<>();
-        profile.put("userId", 1);
-        profile.put("name", "Marusia");
-        profile.put("email", "marusia@example.com");
+    public Mono<Map<String, Object>> profile(@AuthenticationPrincipal OAuth2User oAuth2User, WebSession session) {
+        Map<String, Object> profile = new HashMap<>(oAuth2User.getAttributes());
+//        profile.put("userId", session.getAttribute("userId"));
         return Mono.just(profile);
     }
+
+//    @GetMapping("/api/profile")
+//    public Mono<Map<String, Object>> profile() {
+//        Map<String, Object> profile = new HashMap<>();
+//        profile.put("userId", 1);
+//        profile.put("name", "Marusia");
+//        profile.put("email", "marusia@example.com");
+//        return Mono.just(profile);
+//    }
 }
