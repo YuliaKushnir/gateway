6099f37f3f8d5007a43ce725a4d3ad7234a2e46a
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-02-11 22:02:16 +0200

    Added cors

--- src/main/java/org/example/gateway/config/CorsGlobalConfiguration.java
+++ src/main/java/org/example/gateway/config/CorsGlobalConfiguration.java
@@ -12,8 +12,8 @@ public class CorsGlobalConfiguration {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.addAllowedOrigin("http://34.118.68.15.nip.io");
-        corsConfig.addAllowedOrigin("https://34.118.68.15.nip.io");
+        corsConfig.addAllowedOrigin("http://34.118.111.172/.nip.io");
+        corsConfig.addAllowedOrigin("https://34.118.111.172.nip.io");
         corsConfig.addAllowedMethod("*");
         corsConfig.addAllowedHeader("*");
         corsConfig.setAllowCredentials(true);
