68b433a0ead92e2b4f993d620f3fcc42ee465510
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-02-11 20:14:17 +0200

    Added security

--- src/main/java/org/example/gateway/config/SecurityConfig.java
+++ src/main/java/org/example/gateway/config/SecurityConfig.java
@@ -3,15 +3,18 @@ package org.example.gateway.config;
 //import org.example.gateway.service.OAuth2LoginSuccessHandler;
 //import org.springframework.cloud.gateway.filter.headers.ForwardedHeadersFilter;
 //import org.springframework.cloud.gateway.filter.headers.XForwardedHeadersFilter;
-//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-//import org.springframework.security.config.web.server.ServerHttpSecurity;
-//import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
 //import org.springframework.web.server.adapter.ForwardedHeaderTransformer;
 
-//@EnableWebFluxSecurity
+@EnableWebFluxSecurity
 //@Configuration
 public class SecurityConfig {
 
@@ -21,27 +24,38 @@ public class SecurityConfig {
 //        this.successHandler = successHandler;
 //    }
 //
-//    @Bean
-//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-//        http
-//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-//                .authorizeExchange(exchange -> exchange
-//                        .pathMatchers(
-//                                "/api/login/oauth2/**",
-//                                "/login/oauth2/**",
-//                                "/oauth2/**",
-//                                "/api/user/**",
-//                                "/api/post/**")
-//                        .permitAll()
-//                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-//                        .pathMatchers("/actuator/**", "/health").permitAll()
-//                        .anyExchange().authenticated()
-//                        .anyExchange().permitAll()
-//                );
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers(
+                                "/api/login/oauth2/**",
+                                "/login/oauth2/**",
+                                "/oauth2/**",
+                                "/api/user/**",
+                                "/api/post/**")
+                        .permitAll()
+                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .pathMatchers("/actuator/**", "/health").permitAll()
+                        .anyExchange().authenticated()
+                        .anyExchange().permitAll()
+                )
 //                .oauth2Login(oauth2 -> oauth2
 //                        .authenticationSuccessHandler(successHandler)
 //                );
+                .oauth2Login(oauth2 -> oauth2
+                        .authenticationSuccessHandler((webFilterExchange, authentication) ->
+                                webFilterExchange.getExchange()
+                                        .getResponse()
+                                        .setComplete()
+                                        .then(Mono.fromRunnable(() ->
+                                                webFilterExchange.getExchange().getResponse().getHeaders()
+                                                        .setLocation(URI.create("http://34.118.111.172"))
+                                        ))
+                        )
+                );
 
-//        return http.build();
-//    }
+        return http.build();
+    }
 }
