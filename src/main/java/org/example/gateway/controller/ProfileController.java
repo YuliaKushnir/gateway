5a510c69fd6dab862558c944134f800b969548de
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-02-11 15:42:56 +0200

    Changed return type of controller to Mono

--- src/main/java/org/example/gateway/controller/ProfileController.java
+++ src/main/java/org/example/gateway/controller/ProfileController.java
@@ -4,6 +4,7 @@ package org.example.gateway.controller;
 //import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 //import org.springframework.web.server.WebSession;
 
 import java.util.HashMap;
@@ -20,11 +21,11 @@ public class ProfileController {
 //    }
 
     @GetMapping("/api/profile")
-    public Map<String, Object> profile() {
+    public Mono<Map<String, Object>> profile() {
         Map<String, Object> profile = new HashMap<>();
         profile.put("userId", 1);
         profile.put("name", "Marusia");
         profile.put("email", "marusia@example.com");
-        return profile;
+        return Mono.just(profile);
     }
 }
